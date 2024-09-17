c71b5310e7054e1268ea7da81e043518652fe7fb
sontayaja <sontaya.jahm@gmail.com>
2024-09-17 20:58:56 +0700

    fix schedule card link

--- src/app/components/event-detail-schedule/event-detail-schedule.component.ts
+++ src/app/components/event-detail-schedule/event-detail-schedule.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../container/container.component';
 import { LucideAngularModule, MapPinIcon } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-event-detail-schedule',
     standalone: true,
-    imports: [ContainerComponent, LucideAngularModule],
+    imports: [ContainerComponent, LucideAngularModule, RouterLink],
     templateUrl: './event-detail-schedule.component.html',
     styleUrl: './event-detail-schedule.component.scss',
 })
